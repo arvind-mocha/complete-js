bb96888dcf89ce9af168a748c593b742dace6c5d
arvind l <arvindarvind2210@.com>
2021-02-19 21:43:40 +0530

    'None'

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//promises and fetch api
+
+const request = fetch('https://restcountries.eu/rest/v2/name/india')
+console.log(request)
